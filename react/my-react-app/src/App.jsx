0c03070cb9f7037bdc4b2dc4d4c0804fe6067960
HoldRain47 <holdrain47@gmail.com>
2025-09-26 16:32:48 +0900

    ADD tailwind관련파일

---
+++ react/my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Rule from "./components/JSX/Rule";
+import Interplations from "./components/JSX/Interplations";
+
+export default function App() {
+  return (
+    <div>
+      <Rule></Rule>
+    </div>
+  );
+}
