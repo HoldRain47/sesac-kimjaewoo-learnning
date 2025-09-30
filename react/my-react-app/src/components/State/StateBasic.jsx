62025a362840cf5fbf0ab0a842ae79094d96f26d
HoldRain47 <holdrain47@gmail.com>
2025-09-30 16:25:54 +0900

    UPDATE State 학습

--- react/my-react-app/src/components/State/StateBasic.jsx
+++ react/my-react-app/src/components/State/StateBasic.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+//state는 모듈이 필요함
+import { useState } from "react";
 
 export default function StateBasic() {
-  return <div>StateBasic</div>;
+  const [array, setArray] = useStatus([1, 2, 3]);
+  function handleClick() {
+    const newArray = [...array, 4];
+    setArray(newArray);
+    console.log("상태변겅");
+  }
+  return (
+    <div>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      ></button>
+      {Array.map((e) => {
+        <li>{e}</li>;
+      })}
+    </div>
+  );
 }
