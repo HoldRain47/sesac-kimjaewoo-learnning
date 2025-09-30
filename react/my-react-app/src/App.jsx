1612459c3173e8cf8f39a74d2d4795fc62b2f642
HoldRain47 <holdrain47@gmail.com>
2025-09-30 15:17:12 +0900

    ADD State 추가

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-// import Rule from "./components/JSX/Rule";
-// import Interplations from "./components/JSX/Interplation";
-// import Header from "./components/JSX/Header";
-// import Footer from "./components/JSX/Footer";
+
 // 컴포넌트 불러오기
-import Carts from "./components/Carts/Carts";
+//import Carts from "./components/Carts/Carts";
+import Container from "./components/State/Container";
 export default function App() {
   return (
     <div>
-      {/* 컴포넌트 사용 */}
-      <h1>테스트</h1>
-      <Carts></Carts>
+      <Container></Container>
     </div>
   );
 }
