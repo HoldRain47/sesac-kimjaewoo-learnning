c0560e77266bf6fe274e1a3be603f890ffb90c4b
HoldRain47 <holdrain47@gmail.com>
2025-09-30 15:01:25 +0900

    UPDATE Props3 실습완

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -4,12 +4,13 @@ import React from "react";
 // import Header from "./components/JSX/Header";
 // import Footer from "./components/JSX/Footer";
 // 컴포넌트 불러오기
-import Container from "./components/List/Container";
+import Carts from "./components/Carts/Carts";
 export default function App() {
   return (
     <div>
       {/* 컴포넌트 사용 */}
-      <Container></Container>
+      <h1>테스트</h1>
+      <Carts></Carts>
     </div>
   );
 }
--- react/my-react-app/src/components/Carts/Product.jsx
+++ react/my-react-app/src/components/Carts/Product.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Cart from "./Carts";
 
-export default function Product() {
-  return <div>Product</div>;
+export default function Product({ productE }) {
+  return (
+    <div className="border rounded-lg p-4 shadow-md">
+      <h2>상품명:{productE.title}</h2>
+      <p>가격(price){productE.price}</p>
+      <p>재고(quantity){productE.quantity}</p>
+      <p>총 가격(total){productE.total}</p>
+      <img src={productE.thumbnail} alt="상품 이미지(thumbnail)" />
+    </div>
+  );
 }
