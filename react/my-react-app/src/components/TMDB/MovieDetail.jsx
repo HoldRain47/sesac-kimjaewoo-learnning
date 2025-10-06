02fcafb235fe0c9283d6e5f990fe8c0eb3c3aa47
HoldRain47 <holdrain47@gmail.com>
2025-10-06 23:16:30 +0900

    UPDATE TMDB 업데이트

--- react/my-react-app/src/components/TMDB/MovieDetail.jsx
+++ react/my-react-app/src/components/TMDB/MovieDetail.jsx
@@ -1,49 +1,15 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
-const BASE_URL = `https://api.themoviedb.org/3`;
-const API_KEY = import.meta.env["VITE_TMDB_API_KEY"];
+// 모듈화된 API 요청 함수 불러오기
+import { getPopularMovies } from "./../../api/tmdb";
 
-export default function MovieDetail() {
+export default function MovieList() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const res = await axios(config);
-      const data = res["data"];
-      setMovies(data);
-      console.log(data);
-
-      // console.log(`${Object.keys(res)}`); 출력 결과
-      // MovieDetail.jsx:28 data,status,statusText,headers,config,request
-
-      // console.log(`${Object.keys(res["data"])}`); 출력 결과
-      // adult,
-      //   backdrop_path,
-      //   belongs_to_collection,
-      //   budget,
-      //   genres,
-      //   homepage,
-      //   id,
-      //   imdb_id,
-      //   origin_country,
-      //   original_language,
-      //   original_title,
-      //   overview,
-      //   popularity,
-      //   poster_path,
-      //   production_companies,
-      //   production_countries,
-      //   release_date,
-      //   revenue,
-      //   runtime,
-      //   spoken_languages,
-      //   status,
-      //   tagline,
-      //   title,
-      //   video,
-      //   vote_average,
-      //   vote_count;
+      const res = await getPopularMovies();
+      setMovies(res["results"]);
     }
 
     fetchData();
@@ -51,12 +17,10 @@ export default function MovieDetail() {
 
   return (
     <div>
-      <h1>영화 디테일</h1>
+      <h1>인기 영화 목록</h1>
       <ul>
-        {Object.entries(movies).map(([key, value]) => (
-          <li key={key}>
-            {key}: {JSON.stringify(value)}
-          </li>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
         ))}
       </ul>
     </div>
--- react/my-react-app/src/components/TMDB/MovieSearch.jsx
+++ react/my-react-app/src/components/TMDB/MovieSearch.jsx
@@ -1,61 +1,22 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-const BASE_URL = `https://api.themoviedb.org/3`;
-const API_KEY = import.meta.env["VITE_TMDB_API_KEY"];
+import React from "react";
 
 export default function MovieSearch() {
-  const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const config = {
+        method: "GET",
+        url: `/movie/popular`,
+        params: {
+          page: 1,
+        },
+      };
 
-  async function fetchData() {
-    const config = {
-      method: "GET",
-      url: `${BASE_URL}/search/movie`,
-      headers: {
-        "Content-Type": "application/json",
-        accept: "application/json",
-        Authorization: `Bearer ${API_KEY}`,
-      },
-      params: {
-        language: "ko-KR",
-        query: query,
-      },
-    };
+      const res = await axiosInstance(config);
+      setMovies(res["data"]["results"]);
+    }
 
-    const res = await axios(config);
+    fetchData();
+  }, []);
 
-    setMovies(res["data"]["results"]);
-  }
-
-  function handleChange(event) {
-    setQuery(event["target"]["value"]);
-  }
-
-  return (
-    <div>
-      <h1>영화 검색</h1>
-      <form
-        onSubmit={() => {
-          fetchData();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="영화 검색"
-          value={query}
-          required
-          onChange={(event) => {
-            handleChange(event);
-          }}
-        />
-        <input type="submit" value="검색" />
-      </form>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>MovieSearch</div>;
 }
