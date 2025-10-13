fb09bed057ecb2e4454d560a3f0f4a26abbecac4
HoldRain47 <holdrain47@gmail.com>
2025-10-13 15:05:51 +0900

    UPDATE React Router 실습

--- react/router-app/src/router/index.js
+++ react/router-app/src/router/index.js
@@ -1,21 +1,65 @@
-// src/router/index.js
-
-// createBrowserRouter 함수 불러오기
+// React Router의 createBrowserRouter 불러오기
+// createBrowserRouter 함수
+// 라우터 설정을 생성하는 함수
+// 라우터 : 주소(URL)와 컴포넌트를 매핑
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
 
-// 라우터 생성
+// 레이아웃 컴포넌트 불러오기
+import RootLayout from "../layouts/RootLayOut.jsx";
+import AuthLayout from "../layouts/AuthLayOut.jsx";
+
+// 페이지 컴포넌트 불러오기
+import Home from "../pages/RootPages/Home.jsx";
+import About from "../pages/RootPages/About.jsx";
+import Profile from "../pages/RootPages/Profile.jsx";
+
+import AuthHome from "../pages/AuthPages/AuthHome.jsx";
+import Signup from "../pages/AuthPages/Signup.jsx";
+import Login from "../pages/AuthPages/Login.jsx";
+
+// 라우터 설정 생성
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Home,
+    Component: RootLayout,
+    children: [
+      // 중첩할 자식 경로 객체를 정의하는 배열
+      {
+        index: true, // index: true -> 부모 경로의 기본(root) 경로
+        // 완성되는 path : "/" (부모 경로와 동일)
+        Component: Home,
+      },
+      {
+        path: "about",
+        Component: About,
+      },
+      {
+        path: "profile",
+        Component: Profile,
+      },
+    ],
   },
+  // AuthLayout 경로 설정
   {
-    path: "/about",
-    Component: About,
+    path: "/auth",
+    Component: AuthLayout,
+    children: [
+      {
+        index: true,
+        Component: AuthHome,
+      },
+      // 로그인 경로(path)와 Component 설정
+      {
+        path: "login",
+        Component: Login,
+      },
+      // 회원가입 경로(path)와 Component 설정
+      {
+        path: "signup",
+        Component: Signup,
+      },
+    ],
   },
 ]);
 
-// 라우터 내보내기
 export default router;
